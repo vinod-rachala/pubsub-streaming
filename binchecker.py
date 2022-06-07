9e7278131c176f0149957c53922a80ee052ec9d5
vinod-rachala <37009777+vinod-rachala@users.noreply.github.com>
2022-06-07 01:19:36 -0400

    Add files via upload
    Streaming bin checker data from the Apilayer using pubsub on google cloud platform.

---
+++ binchecker.py
@@ -0,0 +1,63 @@
+# environment variable setup for private key file
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="sa.json"
+
+import json
+from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
+import time
+
+# GCP topic, project & subscription ids
+PUB_SUB_TOPIC = "my-python-topic"
+PUB_SUB_PROJECT = "495490156989"     #"qwiklabs-gcp-00-5167e4946ad4"
+PUB_SUB_SUBSCRIPTION = "my-python-topic-sub"
+
+# Pub/Sub consumer timeout
+timeout = 60.0
+
+# callback function for processing consumed payloads
+# prints recieved payload
+def process_payload(message):
+
+                #print(payload)
+                print(f"Received {message}.")
+                message.ack()
+
+# producer function to push a message to a topic
+def push_payload(payload, topic, project):
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(project, topic)
+        data = json.dumps(payload).encode("utf-8")
+        future = publisher.publish(topic_path, data=data)
+        print("Pushed message to topic.")
+
+# consumer function to consume messages from a topics for a given timeout period
+def consume_payload(project, subscription, callback, period):
+        subscriber = pubsub_v1.SubscriberClient()
+        subscription_path = subscriber.subscription_path(project, subscription)
+        print(f"Listening for messages on {subscription_path}..\n")
+        streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+        # Wrap subscriber in a 'with' block to automatically call close() when done.
+        with subscriber:
+            try:
+                # When `timeout` is not set, result() will block indefinitely,
+                # unless an exception is encountered first.
+                streaming_pull_future.result(timeout=period)
+            except TimeoutError:
+                streaming_pull_future.cancel()
+
+# loop to test producer and consumer functions with a 3 second delay
+while(True):
+    print("===================================")
+    import requests
+    url = "https://api.apilayer.com/bincheck/444444"
+    payload = {}
+    headers= {"apikey": "zh2Twx9UoYc3eeyLfj7CQutaTL2jRlhi"}
+    response = requests.request("GET", url, headers=headers, data = payload)
+    print(response)
+    status_code = response.status_code
+    payload = response.text
+    #print(f"Sending payload: {payload}.")
+    push_payload(payload, PUB_SUB_TOPIC, PUB_SUB_PROJECT)
+    consume_payload(PUB_SUB_PROJECT, PUB_SUB_SUBSCRIPTION, process_payload, timeout)
+    time.sleep(3)
